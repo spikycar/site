b116f756ab52cdb852f299f24a828b3442285ae9
The Unsmooth cats <165571480+spikyneko@users.noreply.github.com>
2024-07-06 07:24:09 +0000

    basic layout

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="px-5">
+        <div className="pt-5">
+          <h1 className="h5 fw-bolder">hello 👋</h1>
+          <p>tbh, this page still under construction.</p>
+        </div>
+        <div className="pt-2">
+          <h2 className="h6 fw-bolder text-muted">--where you can find me</h2>
+          <div className="content">
+          </div>
+        </div>
+        <div className="pt-2">
+          <h2 className="h6 fw-bolder text-muted">--my projects</h2>
+          <div className="content">
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
