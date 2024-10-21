a94a1f996a5aabe8d6ca747c9a74e3718b4de58a
Michael <165571480+spikycar@users.noreply.github.com>
2024-10-21 04:22:57 +0000

    phase 1.5

--- app/component/bottom.tsx
+++ app/component/bottom.tsx
@@ -1,7 +1,7 @@
 export default function BottomBar(){
     return <nav className='navbar bottom'>
         <div className='container justify-content-center'>
-            <p className='muted text-center small m-0'>certified bottom&trade;</p>
+            <p className='muted text-center small m-0'>certified bi&trade;</p>
         </div>
     </nav>;
 }
